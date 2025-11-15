0b5ad688de44af8c38ebe82fdc388513c5d2852a
emoraisdev <evertonsmorais@gmail.com>
2025-11-15 16:35:14 -0300

    feat: Config Sorteio Card

--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -1,12 +1,17 @@
+import { useSelector } from 'react-redux'
 import Card from './Card'
 
 export default props => {
+
+    const { min, max } = useSelector(state => state.intervalo)
+    const randomNumber = parseInt(Math.random() * (max - min)) + min
+
     return (
         <Card title="Sorteio de um Número" purple >
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>8</strong>
+                    <strong>{randomNumber}</strong>
                 </span>
             </div>
         </Card>
